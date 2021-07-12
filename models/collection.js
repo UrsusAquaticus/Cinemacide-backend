faf5535e68f2e39805d6b384738e034e6f4c473d
Zach <Zach.hucker@gmail.com>
2021-07-12 17:37:19 +1200

    Collection fix?

--- models/collection.js
+++ models/collection.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const reviewSchema = new Schema({
+const collectionSchema = new Schema({
 	title: { type: String, required: true },
 	username: { type: String, required: true },
 	createDate: { type: Date, required: true },
 	lastUpdateDate: { type: Date, required: true },
 });
 
-module.exports = mongoose.model("Review", reviewSchema);
+module.exports = mongoose.model("Collection", collectionSchema);
