59ddb77403b64ef164026aee77bfac31e28c6763
Zach <Zach.hucker@gmail.com>
2021-07-12 17:42:45 +1200

    Added Collection model

--- models/collection.js
+++ models/collection.js
@@ -7,6 +7,9 @@ const collectionSchema = new Schema({
 	username: { type: String, required: true },
 	createDate: { type: Date, required: true },
 	lastUpdateDate: { type: Date, required: true },
+	creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+	public: { type: Boolean, required: true },
+	sharedUsers: [{ type: mongoose.Types.ObjectId, required: true, ref: "User" }],
 });
 
 module.exports = mongoose.model("Collection", collectionSchema);
