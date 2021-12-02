35b27196a65ea48ecd0ba33aeff30303de514fbd
Zach <Zach.hucker@gmail.com>
2021-12-02 13:39:58 +1300

    Updated api restrictions

--- middleware/check-api.js
+++ middleware/check-api.js
@@ -2,7 +2,11 @@ const HttpError = require("../models/http-error");
 
 const { readStats, dumpStats } = require("../util/local-storage");
 
-const maxRequestsPerMinute = 100000 / 24 / 60;
+//Keep under max get requests a day
+//cap requests per hour
+const maxCallsPerDay = 1000;
+const refreshRate = 1000 * 60 * 60; // an hour
+const maxRequestsPerRefresh = maxCallsPerDay / 24; //maxCalls / 24 hours
 
 module.exports = (req, res, next) => {
     if (req.method === "OPTIONS") {
@@ -15,20 +19,20 @@ module.exports = (req, res, next) => {
         apiCount = apiCount + 1 || 1;
         console.log("API Request: #" + apiCount);
 
-        expDate = expDate || new Date().getTime() + 1000 * 60;
+        expDate = expDate || new Date().getTime() + refreshRate;
 
         const remainingTime = new Date(expDate).getTime() - new Date().getTime();
         if (remainingTime <= 0) {
             console.log("Api Count Refreshed");
             apiCount = 0;
-            expDate = new Date().getTime() + 1000 * 60;
+            expDate = new Date().getTime() + refreshRate;
         }
 
         dumpStats({ apiCount, expDate });
 
-        if (apiCount > maxRequestsPerMinute) {
-            console.log("Api request limit exceeded.");
-            throw new Error("Api request limit exceeded.");
+        if (apiCount > maxRequestsPerRefresh) {
+            console.log("Api request limit exceeded. Try again Later.");
+            throw new Error("Api request limit exceeded. Try again Later.");
         }
 
         next();
